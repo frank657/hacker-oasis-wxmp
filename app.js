be68041b1fdb1d40af2baaee941cddd191cbfac5
nicole gerbauld <nicole.gerbauld@gmail.com>
2019-08-27 15:23:33 +0800

    done

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+    const host = "http://localhost:3000"
+    wx.login({
+      success: (res) => {
+        console.log(res)
+      }
+    })
 
     // 登录
     wx.login({
--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -5,15 +5,37 @@ Page({
    * Page initial data
    */
   data: {
-
+    projects: []
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    console.log('IN onLoad')
+    let page = this;
+      wx.request({
+        url: 'http://localhost:3000/api/v1/projects',
+        success: function(res) {
+          console.log(111, res.data);
+          
+          page.setData(res.data);
+        }
+      })
+    },
+    clickMe:function(e) {
+      console.log(e);
+      wx.request({
+        url: 'http://localhost:3000/api/v1/projects',
+        sucess: function (res) {
+          console.log(111, res.data);
 
-  },
+          page.setData({
+            projects: projects
+          });
+        }
+      })
+    },
 
   /**
    * Lifecycle function--Called when page is initially rendered
