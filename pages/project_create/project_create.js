0b7738255650ad93ac48e43d59bb8144de50cb55
Frank Zhu <qf.zhu.frank@gmail.com>
2019-08-30 17:37:00 +0800

    stuff

---
+++ pages/project_create/project_create.js
@@ -0,0 +1,73 @@
+// pages/project_create/project_create.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    scroll: 0
+  },
+
+  scrollSteps() {
+    this.setData({
+      scroll: this.data.scroll == 4 ? 0 : this.data.scroll + 1
+    })
+    console.log(this.data.scroll)
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
