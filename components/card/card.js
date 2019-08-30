7885945024efd8de169d51bf455f0a0504a06b17
Jimmy Chen <jimmyjchen@outlook.com>
2019-08-30 18:55:43 +0800

    linked project show page data

--- components/card/card.js
+++ components/card/card.js
@@ -23,9 +23,13 @@ Component({
   methods: {
     clickMe: function (event) {
       console.log(event);
+      const id = event.currentTarget.dataset.id;
       wx.navigateTo({
-        url: '../../pages/project_show/project_show'
+        url: `../../pages/project_show/project_show?id=${id}`
       })
+    },
+    goToMap: function(event) {
+      console.log(event);
     }
   }
 })
